e405afb172128ff0dffa90ba6d2e61c7d619b12b
TOMINJOSE88 <tominjose123@gmail.com>
2025-03-18 15:53:53 +1100

    week 2

---
+++ week2/task2_1P/src/main/java/sit707_week2/Main.java
@@ -0,0 +1,14 @@
+package sit707_week2;
+
+/**
+ * Hello world!
+ * @author Ahsan Habib
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        SeleniumOperations.w3school_signup_page("https://profile.w3schools.com/signup?redirect_url=https%3A%2F%2Fwww.w3schools.com%2F");
+    }
+}
